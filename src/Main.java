abb191391f06a176876a3f64dcae64bdcf52dda5
Matthias Riener <rienermatthias@gmail.com>
2020-03-23 15:57:26 +0100

    added custom fonts with google font api

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,27 @@
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+
 import javafx.scene.Scene;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        TextFlow flow = new TextFlow();
+        flow.setPrefWidth(750);
+        final Text t1 = new Text("Hello @FontFace - Grand Hotel.");
+        t1.setStyle("-fx-font-family: 'Grand Hotel'; -fx-font-size: 80;");
+        flow.getChildren().add(t1);
+        final Text t2 = new Text("Hello @FontFace - New Rocker");
+        t2.setStyle("-fx-font-family: 'New Rocker'; -fx-font-size: 80; -fx-fill: orange;");
+        flow.getChildren().add(t2);
+        Scene scene = new Scene(flow);
+        scene.getStylesheets().add("http://fonts.googleapis.com/css?family=New+Rocker");
+        scene.getStylesheets().add("http://fonts.googleapis.com/css?family=Grand+Hotel");
+        primaryStage.setTitle("Hello @FontFace");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
