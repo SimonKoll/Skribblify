f51be88c956950f71d4719a8f5a33fa59784d51d
Matthias Riener <rienermatthias@gmail.com>
2020-03-24 13:03:56 +0100

    displaying right font

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import com.google.api.services.sheets.v4.SheetsScopes;
+import fonts.SheetsQuickstart;
 import javafx.application.Application;
 
 import javafx.scene.Scene;
@@ -6,23 +8,11 @@ import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+    private static SheetsQuickstart SheetsQuickStart;
     @Override
     public void start(Stage primaryStage) throws Exception {
-        TextFlow flow = new TextFlow();
-        flow.setPrefWidth(750);
-        final Text t1 = new Text("Hello @FontFace - Grand Hotel.");
-        t1.setStyle("-fx-font-family: 'Grand Hotel'; -fx-font-size: 80;");
-        flow.getChildren().add(t1);
-        final Text t2 = new Text("Hello @FontFace - New Rocker");
-        t2.setStyle("-fx-font-family: 'New Rocker'; -fx-font-size: 80; -fx-fill: orange;");
-        flow.getChildren().add(t2);
-        Scene scene = new Scene(flow);
-        scene.getStylesheets().add("http://fonts.googleapis.com/css?family=New+Rocker");
-        scene.getStylesheets().add("http://fonts.googleapis.com/css?family=Grand+Hotel");
-        primaryStage.setTitle("Hello @FontFace");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+
+        SheetsQuickstart.loadConfiguration(primaryStage);
     }
 
 
--- src/main/java/fonts/SheetsQuickstart.java
+++ src/main/java/fonts/SheetsQuickstart.java
@@ -26,6 +26,16 @@ import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.ValueRange;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -35,12 +45,13 @@ import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SheetsQuickstart {
     private static final String APPLICATION_NAME = "Google Sheets API Java Quickstart";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String TOKENS_DIRECTORY_PATH = "tokens";
-    public static HashMap<String, String> fontFamilyLink = new HashMap<String, String>();
+    public static Map<String, String> fontFamilyLink = new HashMap<>();
 
     /**
      * Global instance of the scopes required by this quickstart.
@@ -82,7 +93,7 @@ public class SheetsQuickstart {
     public static void loadData() throws GeneralSecurityException, IOException {
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
         final String spreadsheetId = "1NiTFnRnopQNbLcw7XilRAFn4p5T-hFUAqEg24oK9ZKw";
-        final String range = "A2:E";
+        final String range = "A1:E";
         Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
@@ -96,19 +107,49 @@ public class SheetsQuickstart {
             System.out.println("Font-Link");
             for (List row : values) {
                 String [] fontFamily = row.get(0).toString().split("=")[1].split("&");
-                fontFamilyLink.put(fontFamily[0], row.get(0).toString() );
+
+                fontFamilyLink.put(fontFamily[0].replaceAll("\\+", " "), row.get(0).toString() );
             }
         }
     }
 
-    public static void showEntries(){
-        fontFamilyLink.forEach((k,v) -> System.out.println(k + ":" + v));
+
+
+
+
+    public static void loadConfiguration(Stage primaryStage) throws GeneralSecurityException, IOException {
+        loadData();
+         displayHashMap(primaryStage);
+
+
+
     }
 
-    public static void main(String... args) throws IOException, GeneralSecurityException {
-        // Build a new authorized API client service.
-        loadData();
-        showEntries();
+
+    public static void displayHashMap(Stage stage){
+        TextFlow flow = new TextFlow();
+        flow.setPrefWidth(750);
+
+        for (Map.Entry<String, String> entry : fontFamilyLink.entrySet()) {
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+            final Text key = new Text(entry.getKey());
+
+            key.setStyle("-fx-font-family: " + entry.getKey() + "; -fx-font-size:80;");
+            //t2.setStyle("-fx-font-family: 'New Rocker'; -fx-font-size: 80; -fx-fill: orange;");
+            flow.getChildren().add(key);
+        }
+
+
+        Scene scene = new Scene(flow);
+        for(String value : fontFamilyLink.values()){
+            System.out.println("Lol");
+            scene.getStylesheets().add(value);
+        }
+
+        stage.setTitle("Hello @FontFace");
+        stage.setScene(scene);
+        stage.show();
+
     }
 
 
