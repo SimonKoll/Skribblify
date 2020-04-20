9ae1af0522c8b4fef5fd7f0f9889503f1ec35858
SimonKoll <simonkoll20@gmail.com>
2020-04-20 13:04:41 +0200

    restructuree

---
+++ src/main/java/login/Controller.java
@@ -0,0 +1,23 @@
+package login;
+
+import java.net.URL;
+import java.util.*;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+import javafx.scene.layout.AnchorPane;
+
+public class Controller {
+    @FXML
+    private AnchorPane rootPane;
+
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+
+    @FXML
+    private void loadRegister(ActionEvent event) {
+
+    }
+
+}
