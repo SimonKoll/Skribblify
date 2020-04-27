48b644a5a9fbd7aa278b6c43c0f4d42588eb34e1
TristanHill <jezza@gmx.at>
2020-04-27 16:18:02 +0200

    ragister (kann sein dass alles broken is aber naja ¯\_(ツ)_/¯)

--- src/main/java/login_registration/registration/viewController/RegisterC.java
+++ src/main/java/login_registration/registration/viewController/RegisterC.java
@@ -9,9 +9,16 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import login_registration.login.viewController.LoginC;
+import login_registration.model.User;
 
+import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class RegisterC implements Initializable {
     @FXML
@@ -28,18 +35,101 @@ public class RegisterC implements Initializable {
     @FXML
     private Button goToLoginBtn;
 
+    private Statement statement;
+    private User model;
 
+
+    public RegisterC() {
+    }
 
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 
     @FXML
-    private void register_pressed(ActionEvent event) {
+    private void register_pressed(ActionEvent event) throws SQLException {
+        register();
     }
 
     @FXML
     private void goToLogin(ActionEvent event) {
     }
+
+    private void register() throws SQLException {
+        String newusername = username.getText();
+        String newpassword_1 = password_1.getText();
+        String newpassword_2 = password_2.getText();
+
+
+        if(newpassword_1 == newpassword_2){
+            User.newToDB(newusername,newpassword_1, this.statement);
+
+        }
+
+    }
+
+    public static void show(Stage stage, Statement statement){
+
+        // Animation test
+
+        try{
+
+
+
+            FXMLLoader loader = new FXMLLoader(LoginC.class.getClassLoader().getResource("login_registration.registration/register.fxml"));
+            Parent root = (Parent) loader.load();
+
+            Scene scene = new Scene(root);
+
+
+            if(stage == null){
+                stage = new Stage();
+            }
+
+            stage.setScene(scene);
+            stage.setTitle("Skribblify - Register");
+
+            RegisterC registerC = (RegisterC) loader.getController();
+            registerC.statement = statement;
+
+            registerC.model = new User();
+            stage.show();
+
+        }catch (IOException ex){
+            Logger.getLogger(LoginC.class.getName()).log(Level.SEVERE, null, ex);
+            System.err.println("Something wrong with PersonV.fxml!");
+            ex.printStackTrace(System.err);
+            System.exit(1);
+        }
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
